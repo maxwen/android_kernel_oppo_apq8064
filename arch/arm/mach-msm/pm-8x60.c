39bdc7f3552b3a31681c0ea6d7db8c0470845f03
Venkat Devarasetty <vdevaras@codeaurora.org>
2013-06-03 16:15:48 +0530

    msm: pm: send notification only for SPC and PC
    Today the pm notification is sent for all the power
    modes of each CPU. This is not necessary for shallow
    power modes.
    
    This change is to send the notification for only
    standalone power collapse for each core and idle
    power collapse of core0.
    
    Change-Id: I9b3df3d72e43d645387c94b1b9c5da3bcf2f2e5f
    Signed-off-by: Venkat Devarasetty <vdevaras@codeaurora.org>

--- arch/arm/mach-msm/pm-8x60.c
+++ arch/arm/mach-msm/pm-8x60.c
@@ -54,6 +54,7 @@
 #include "timer.h"
 #include "pm-boot.h"
 #include <mach/event_timer.h>
+#include <linux/cpu_pm.h>
 
 /* OPPO 2013-04-19 hewei Add begin for ftm */
 enum{
@@ -549,6 +550,9 @@ static bool __ref msm_pm_spm_power_collapse(
 		pr_info("CPU%u: %s: notify_rpm %d\n",
 			cpu, __func__, (int) notify_rpm);
 
+	if (from_idle == true)
+		cpu_pm_enter();
+
 	ret = msm_spm_set_low_power_mode(
 			MSM_SPM_MODE_POWER_COLLAPSE, notify_rpm);
 	WARN_ON(ret);
@@ -586,6 +590,10 @@ static bool __ref msm_pm_spm_power_collapse(
 
 	ret = msm_spm_set_low_power_mode(MSM_SPM_MODE_CLOCK_GATING, false);
 	WARN_ON(ret);
+
+	if (from_idle == true)
+		cpu_pm_exit();
+
 	return collapsed;
 }
 
