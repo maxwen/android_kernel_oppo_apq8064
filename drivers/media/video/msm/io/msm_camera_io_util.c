a285889a452cb1bf906508e2885410770e8cf238
maxwen <max.weninger@gmail.com>
2013-08-25 00:26:16 +0200

    Revert "oppo: drivers/media/video/msm/io"
    This reverts commit 321c47ef5d9c692749014eccf86287a4eebe62bc.

--- drivers/media/video/msm/io/msm_camera_io_util.c
+++ drivers/media/video/msm/io/msm_camera_io_util.c
@@ -326,12 +326,6 @@ int msm_camera_enable_vreg(struct device *dev, struct camera_vreg_t *cam_vreg,
 					continue;
 			} else
 				j = i;
-/* OPPO 2013-05-23 yxq Add begin for reason */
-            if (reg_ptr[j] == NULL) {
-                printk("%s reg_ptr[%d] is null\n", __func__, j);
-                continue;
-            }
-/* OPPO 2013-05-23 yxq Add end */
 			regulator_disable(reg_ptr[j]);
 			if (cam_vreg[j].delay > 20)
 				msleep(cam_vreg[j].delay);
